21b8d992cbc0a4f7cf20bd6afa8648f61811fcbb
eddie <eddie.p.vasquez@gmail.com>
2014-11-15 16:22:30 -0600

    twerkin

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,8 @@
 #!/bin/python
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+
 
 # Configuration:
 DATABASE = '/tmp/flaskr.db'
@@ -19,6 +21,12 @@ app.config.from_object(__name__)
 # Connection to database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+# Initialize sqlite database
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 # Start App
 if __name__ == '__main__':
